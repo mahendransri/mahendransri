ed96931b7f663fd66b8dd32945df4b122ccbbb32
mahendransri <mahendransri999@gmail.com>
2023-08-11 12:36:00 +0530

    Task Two done

---
+++ src/org/demoo/seleniumtry.java
@@ -0,0 +1,12 @@
+package org.demoo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class seleniumtry {
+	public static void main(String[] args) {
+		WebDriver driver = new FirefoxDriver();
+		
+	}
+
+}
